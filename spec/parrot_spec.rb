9c40837024b12aa4a78eaa8dae350bfa9056c206
Jazmin M <jazminmerckel@gmail.com>
2019-09-20 01:09:11 +0000

    Automatically backed up by Learn

--- spec/parrot_spec.rb
+++ spec/parrot_spec.rb
@@ -2,8 +2,9 @@ require_relative './spec_helper'
 require_relative '../parrot.rb' # Code your solution in this file
 
 def parrot(phrase = "Squawk!")
+  puts parrot
 end
-parrot
+
 
 
 describe '#parrot' do
