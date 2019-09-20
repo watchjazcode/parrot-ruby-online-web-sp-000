03f5a07e73f9a636d8301b6212495ea0df00a409
Jazmin M <jazminmerckel@gmail.com>
2019-09-20 01:02:41 +0000

    Automatically backed up by Learn

--- spec/parrot_spec.rb
+++ spec/parrot_spec.rb
@@ -1,7 +1,7 @@
 require_relative './spec_helper'
 require_relative '../parrot.rb' # Code your solution in this file
 
-def parrot(bird_goes = "Squawk!")
+def parrot(phrase = "Squawk!")
   puts "Squawk!"
 
 end
