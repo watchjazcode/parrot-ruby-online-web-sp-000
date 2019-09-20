5fa8a5852215f62c7470a37b77f8123f7b60a676
Jazmin M <jazminmerckel@gmail.com>
2019-09-20 00:57:32 +0000

    Automatically backed up by Learn

--- spec/parrot_spec.rb
+++ spec/parrot_spec.rb
@@ -2,9 +2,9 @@ require_relative './spec_helper'
 require_relative '../parrot.rb' # Code your solution in this file
 
 def parrot(string)
-  
+  "Squawk"
   puts "Squawk!"
-
+end
 
 describe '#parrot' do
   it 'should output "Squawk!" to the terminal when called without any arguments' do
