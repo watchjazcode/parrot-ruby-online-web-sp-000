104859ebf38c196a0d247388394d8513e8a6d5fe
Jazmin M <jazminmerckel@gmail.com>
2019-09-20 01:19:45 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot(phrase= "Squawk!")
-  puts "#{phrase}"
-  return phrase
+def parrot(phrase= "Squawk!") #defining method with a default argument
+  puts "#{phrase}" #i'm outputting the argument
+  return phrase #i'm returning the argument #test says "pretty bird, that is how it knows"
 end
