efb33fa0db73fcc47a3724a65daaa032aa81151d
Jazmin M <jazminmerckel@gmail.com>
2019-09-20 01:15:58 +0000

    Automatically backed up by Learn

--- spec/parrot_spec.rb
+++ spec/parrot_spec.rb
@@ -1,12 +1,6 @@
 require_relative './spec_helper'
 require_relative '../parrot.rb' # Code your solution in this file
 
-def parrot(phrase= "Squawk!")
-  puts "#{phrase}"
-  return
-end
-
-
 describe '#parrot' do
   it 'should output "Squawk!" to the terminal when called without any arguments' do
     expect($stdout).to receive(:puts).with("Squawk!")
